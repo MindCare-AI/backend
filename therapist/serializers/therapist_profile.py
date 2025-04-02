72dc3c46249d504ac51db4004f50f02de9349752
AzizBahloul <azizbahloul3@gmail.com>
2025-04-02 23:11:27 +0100

    add uui directly into teh profile enpoint

--- therapist/serializers/therapist_profile.py
+++ therapist/serializers/therapist_profile.py
@@ -7,14 +7,24 @@ class TherapistProfileSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     profile_completion_percentage = serializers.IntegerField(read_only=True)
     is_profile_complete = serializers.BooleanField(read_only=True)
-    username = serializers.SerializerMethodField()  # Add this line
+    username = serializers.SerializerMethodField()
+    # Make these fields writable by removing read_only and adding required=False
+    first_name = serializers.CharField(source='user.first_name', required=False)
+    last_name = serializers.CharField(source='user.last_name', required=False)
+    email = serializers.EmailField(source='user.email', read_only=True)
+    phone_number = serializers.CharField(source='user.phone_number', read_only=True)
 
     class Meta:
         model = TherapistProfile
         fields = [
             "id",
+            "unique_id",  # Add this field
             "user",
-            "username",  # Add this line
+            "username",
+            "first_name",  
+            "last_name",   
+            "email",
+            "phone_number",
             "specialization",
             "license_number",
             "years_of_experience",
@@ -33,13 +43,27 @@ class TherapistProfileSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = [
             "id",
+            "unique_id",  # Add this field
             "user",
-            "username",  # Add this line
+            "username",
+            "email",
+            "phone_number",
             "created_at",
             "updated_at",
             "profile_completion_percentage",
             "is_profile_complete",
         ]
 
-    def get_username(self, obj):  # Add this method
-        return obj.user.username  # Access the username through the User model
+    def update(self, instance, validated_data):
+        # Handle nested user data (first_name, last_name)
+        user_data = validated_data.pop('user', {})
+        if user_data:
+            user = instance.user
+            for attr, value in user_data.items():
+                setattr(user, attr, value)
+            user.save()
+
+        return super().update(instance, validated_data)
+
+    def get_username(self, obj):
+        return obj.user.username
